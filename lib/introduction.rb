e224be9f2053cf08f1c061339332df9ed9eabc68
Jakarai Forsythe <jakaraiforsythe@gmail.com>
2019-10-10 19:40:03 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -11,7 +11,9 @@ introduction("Dan")
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
-introduction_with_language()
+introduction_with_language("Dan", "Ember")
+introduction_with_language("Josh", "React")
+def introduction_with_language_optional()
 
 
 
